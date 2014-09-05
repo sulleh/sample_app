6b8f695079d9946f7e22f88acff5e6a2229ab14c
sulleh <mike.osullivan@gmail.com>
2014-09-04 23:37:58 -0400

    added an about page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,10 +24,10 @@ describe "Static pages" do
 			visit '/static_pages/help'
 			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
 		end
-
 	end
 
 	describe "About page" do 
+
 		it "should have the content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content('About Us')
@@ -39,6 +39,15 @@ describe "Static pages" do
 		end
 	end
 
+	describe "Contact" do 
+		it "should have the content 'Contact Me'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content("Contact Me")
+		end
 
-
+		it "should have the right title" do 
+			visit '/static_pages/contact' 
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Me")
+		end
+	end
 end
