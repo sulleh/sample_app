df9a9cf946243c6a584e5552bd229c904b6e42b7
sulleh <mike.osullivan@gmail.com>
2014-12-07 23:03:52 -0500

    added user following part deux

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,9 +39,6 @@ class UsersController < ApplicationController
   end
 
 
-
-
-
 def create
 	@user = User.new(user_params) 
 	if @user.save
@@ -52,6 +49,24 @@ def create
 		render 'new'
 	end
 end
+
+def following
+  @title = "Following"
+  @user = User.find(params[:id])
+  @users = @user.followed_users.paginate(page: params[:page])
+  render 'show_follow'
+end
+
+def followers
+  @title = "Followers"
+  @user = User.find(params[:id])
+  @users = @user.followers.paginate(page: params[:page])
+  render 'show_follow'
+end
+
+
+
+
 
 	private
 
